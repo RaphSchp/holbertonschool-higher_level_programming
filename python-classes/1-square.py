8c24d49c0fed7bd531049797c34017e90f69cb01
RaphSchp <raphaelschopfer@gmail.com>
2023-10-11 12:56:30 +0000

    add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+class Square:
+    """A Square"""
+
+    def __init__(self, size):
+        """size is the size of the square"""
+        self.__size = size

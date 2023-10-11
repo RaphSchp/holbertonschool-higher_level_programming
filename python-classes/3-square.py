c6d8291adbb543bad209df1c07385dcc734af9e2
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-10-11 15:41:43 +0000

    add 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """
+            TypeError: If `size` type is not `int`.
+
+            ValueError: If `size` is less than `0`.
+
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2

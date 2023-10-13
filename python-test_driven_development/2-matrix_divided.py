c4717a0289c00c44fe5fedcbbd2dcfd400cf3c5f
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-10-13 09:56:51 +0000

    fix indentation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,52 +1,50 @@
 #!/usr/bin/python3
+"""This is a module that contains a function
 
-"""
-Creating a funtion that divides the matrix
-
+This function is going to be used to make the project of TDD
 """
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix.
+    """Divide all elements of a list
 
-    Parameters:
-    matrix: matrix being divided
-    div: number the matrix is divided by.
 
-    Returns: a new divided matrix
+    Args:
+        matrix (list): List of lists of integers or floats
+        div (int, float): The number to be divided
 
-    Raises:
-    TypeError: If matrix is not a list of integers or floats.
-    TypeError: If each row of the matrix does not have the same size.
-    TypeError: If div is not a number (integer or float).
-    ZeroDivisionError: If div is equal to 0.
+    Returns:
+        A new matrix
+
     """
 
-    # Validate matrix
-
-    if not isinstance(matrix, list) or \
-            not all(isinstance(row, list) for row in matrix):
+    if type(matrix) != list:
         raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+                "matrix must be a matrix (list of lists) of integers/floats")
+    length = len(matrix[0])
+    for lista in matrix:
+        if len(lista) != length:
+            raise TypeError("Each row of the matrix must have the same size")
+        elif type(lista) != list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
+    else:
+        temp = [num for lista in matrix for num in lista]
+        error = None
+        for num in temp:
+            if type(num) != int and type(num) != float:
+                error = num
+                break
+        if error:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
 
-    # Validate RowSize
-
-    RowSize = len(matrix[0])
-    if not all(len(row) == RowSize for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    # Validate div
-
-    if not isinstance(div, (int, float)):
+    if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Perform division and rounding
-
-    NewMatrix = []
-    for row in matrix:
-        NewRow = [round(element / div, 2) for element in row]
-        NewMatrix.append(NewRow)
-
-    return NewMatrix
+    new_matrix = [[round(num / div, 2) for num in lista] for lista in matrix]
+    return new_matrix
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
+"""This is a module that contains a function
 
-"""
-Function to print Name
+This function is going to be used to make the project of TDD
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """Print a custom string
+
+    Print a first name with its respective last name
+
+    Args:
+        first_name (str): The first name
+        last_name (str): The last name
+
     """
-    Prints "My name is first name last name".
-
-    Parameters:
-    first_name: The first name
-    last_name: The last name
-
-    Raises:
-    TypeError: If first_name is not a string or last_name is not a string.
-    """
-
-    # Validate first_name and last_name
-
-    if not isinstance(first_name, str):
-        raise TypeError(
-            "first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) != str:
         raise TypeError("last_name must be a string")
-    else:
-        print(f"My name is {first_name} {last_name}")
+
+    print(f"My name is {first_name} {last_name}")

5ecb40c0e626dab1fe809f8fdd616a29bcd5d2a0
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-10-18 12:10:11 +0000

    fix docidoci

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-'''Class'''
+'''MyList class with a print_sorted method'''
 
 class MyList(list):
+    '''A custom list class that extends the built-in list class.'''
+    
     def print_sorted(self):
-        '''Print sorted list'''
+        '''Print the elements of the list in sorted order.
+
+        This method sorts the list in ascending order and prints it.
+        '''
         print(sorted(self))

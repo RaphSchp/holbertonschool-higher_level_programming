7d59daeb832ca4ea60ea31e2688936321a4672a4
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-10-19 14:06:53 +0000

    fix indentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-'''MyList class with a print_sorted method'''
+""" MyList class inherited from list - sorts input """
 
 
 class MyList(list):
-    '''A custom list class that extends the built-in list class.'''
-
+    """ MyList class inherited from list - sorts input """
     def print_sorted(self):
-        '''Print the elements of the list in sorted order.
-
-        This method sorts the list in ascending order and prints it.
-        '''
+        """ prints sorted list """
         print(sorted(self))

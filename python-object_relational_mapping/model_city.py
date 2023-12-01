b8689d7d4ffe91bffa7de28ea8c8515305f2f622
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-12-01 16:24:06 +0000

    fix model

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-# Defines a City model.
-# Inherits from SQLAlchemy Base and links to the MySQL table cities.
+"""Module that contains the class definition of a City """
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base, State
+import MySQLdb
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
+    """ def class City """
+    __tablename__ = 'cities'
 
-    Attributes:
-        id (str): The city's id.
-        name (sqlalchemy.Integer): The city's name.
-        state_id (sqlalchemy.String): The city's state id.
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+    )
+
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id"),
+        nullable=False
+    )
+
+
+if __name__ == "__main__":
+    connection = MySQLdb(
+        host='localhost',
+        port=3306
+    )
     
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
-
+"""a python file that contains the class definition
+of a State and an instance Base = declarative_base()"""
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
+from sys import argv
 Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
-    """
+    """claas state that inherits from base"""
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
+    
